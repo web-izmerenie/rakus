d3eb324cce0e2c3f1df39fab7a5b04de0f804435
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-07-07 17:30:43 +0300

    Start project!

---
+++ bitrix/templates/main/header.php
@@ -0,0 +1,16 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?$APPLICATION->ShowHead();?>
+		<title><?$APPLICATION->ShowTitle();?></title>	
+	</head>
+	<body>
+		<div id="panel">
+			<?$APPLICATION->ShowPanel();?>
+		</div>
+	
+						
