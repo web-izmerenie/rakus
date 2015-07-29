795c00c919d3e2fb82c93dfe46ca342d523f1b2f
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-07-29 14:15:48 +0300

    Edit content

--- index.php
+++ index.php
@@ -7,19 +7,19 @@ $APPLICATION->SetTitle("Rakurs-rent");
 	<h3>под любые задачи</h3>
 	<ul>
 		<li>
+			<div class="image"><img src="<?=$tplPath?>images/icon-2.png" alt="" />
+			</div>
+			<div class="hover_text"><span>Доставим оборудование на объект в сжатые сроки. По Ростову-на-Дону и Краснодару — в течение 2 часов.</span>
+			</div>
+			<div class="title"><span>Просто</span></div>
+		</li>
+		<li>
 			<div class="main-block">
 				<div class="image"><img src="<?=$tplPath?>images/icon-1.png" alt="" /></div>
 				<div class="title"><span>Быстро</span></div>
 			</div>
 			<div class="hover_text"><span>Вы позвонили — мы приехали. Оформление договора аренды у вас на объекте без дополнительных согласований.</span>
 			</div>
-		</li>
-		<li>
-			<div class="image"><img src="<?=$tplPath?>images/icon-2.png" alt="" />
-			</div>
-			<div class="hover_text"><span>Доставим оборудование на объект в сжатые сроки. По Ростову-на-Дону и Краснодару — в течение 2 часов.</span>
-			</div>
-			<div class="title"><span>Просто</span></div>
 		</li>
 		<li>
 			<div class="image"><img src="<?=$tplPath?>images/icon-3.png" alt="" /></div>
